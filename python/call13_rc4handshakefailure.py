1ed1f973874722f0a9227361545bbb2a8c5a3a4c
Dennis Knorr <dennis.knorr@gmx.net>
2021-03-22 19:54:24 +0100

    moving around some calls

---
+++ python/call13_rc4handshakefailure.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import requests
+
+# site: https://badssl.com
+# state should be: good
+# verification: disabled
+# comment: warning on command line output on disabled verification
+
+badssl_startsite = "https://rc4.badssl.com"
+https_verification = False
+
+def verification_on():
+    if https_verification:
+        print("Verification is enabled")
+    else:
+        print("Verification is disabled")
+
+def test_disabled_verification_with_warning():
+    try:
+        print("Starting a request to " + badssl_startsite)
+        requests.get(badssl_startsite, verify = https_verification)
+        # This should output something like the following message:
+        # InsecureRequestWarning: Unverified HTTPS request is being made to host 'badssl.com'. 
+        # Adding certificate verification is strongly advised. 
+        # See: https://urllib3.readthedocs.io/en/latest/advanced-usage.html#ssl-warnings
+    except Exception as e:
+        print("There was an error requesting " + badssl_startsite)
+        print("Error:"+str(e))
+        return
+    print("Request returned successfully")
+
+if __name__ == "__main__":
+    verification_on()
+    test_disabled_verification_with_warning()
+
